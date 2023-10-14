0d1e77de5aa09f2300aa57f957a45166b44aa6bd
edsaugusto <edsonjota@hotmail.com>
2023-10-14 12:46:47 -0300

    primeiro commit

---
+++ src/test/java/com/cursoSpring/lojaDeLivros/LojaDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoSpring.lojaDeLivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
