7e7f85922cec21a20b6a61ee18c1e09083fd554e
edsaugusto <edsonjota@hotmail.com>
2023-10-16 19:20:44 -0300

    adicionando notations

---
+++ lojaDeLivros/src/main/java/com/cursoSpring/lojaDeLivros/dominio/Livro.java
@@ -0,0 +1,128 @@
+package com.cursoSpring.lojaDeLivros.dominio;
+
+import java.io.Serializable;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Livro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String nomeAutor;
+    private String texto;
+
+    private Categoria categoria;
+
+    public Livro(Long id, String titulo, String nomeAutor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nomeAutor = nomeAutor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Livro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro [id=" + id + ", titulo=" + titulo + ", nomeAutor=" + nomeAutor + ", texto=" + texto
+                + ", categoria=" + categoria + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+        result = prime * result + ((nomeAutor == null) ? 0 : nomeAutor.hashCode());
+        result = prime * result + ((texto == null) ? 0 : texto.hashCode());
+        result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Livro other = (Livro) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (titulo == null) {
+            if (other.titulo != null)
+                return false;
+        } else if (!titulo.equals(other.titulo))
+            return false;
+        if (nomeAutor == null) {
+            if (other.nomeAutor != null)
+                return false;
+        } else if (!nomeAutor.equals(other.nomeAutor))
+            return false;
+        if (texto == null) {
+            if (other.texto != null)
+                return false;
+        } else if (!texto.equals(other.texto))
+            return false;
+        if (categoria == null) {
+            if (other.categoria != null)
+                return false;
+        } else if (!categoria.equals(other.categoria))
+            return false;
+        return true;
+    }
+
+}
